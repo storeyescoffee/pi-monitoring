59219f8b166ce70d796381520f297fb5a4c65d5a
storeyescoffee <storeyes.morocco@gmail.com>
2026-02-04 17:36:24 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install the required dependencies
+sudo apt update
+sudo apt install mosquitto-clients jq -y
+
+# grant execution permission on main.sh
+chmod +x main.sh
+
+# schedule the crontab 
+CRON_LINE="* * * * * ~/pi-monitoring/main.sh"
+(crontab -l 2>/dev/null | grep -F "$CRON_LINE") >/dev/null 2>&1 || \
+(crontab -l 2>/dev/null; echo "$CRON_LINE") | crontab -
